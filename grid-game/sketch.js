86e0cd86c601e0052f6ffb609de217a14226c21d
arushmitra <131355540+arushmitra@users.noreply.github.com>
2024-04-30 00:38:09 -0600

    final

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -1,4 +1,5 @@
-// Impossible Mission 1984
+// Arush Mitra
+// 2D Assignment
 
 let grid;
 let cellSize;
@@ -7,6 +8,10 @@ const PLAYER = 9;
 const OPEN_TILE = 0;
 const IMPASSIBLE = 1;
 const GOAL = 2;
+const FINISH = 3;
+const BLOCK = 4; 
+const FINISHLINE = 5; 
+// setting the positions 
 let player = {
   x: 0,
   y: 0,
@@ -15,214 +20,225 @@ let goal = {
   x: 1,
   y: 1,
 }
+let finish = {
+  x: 4,
+  y: 4,
+};
+let block = { 
+  y: 4,
+};
 let grassImg;
 let pavingImg;
 let bgMusic;
 let cantWalk;
 let heroSprite; 
 let blockToMove;
+let finishLine;
 let state = "start screen";
 let level1;
+// instead of using json file (because mine had some sort of fault) i used the data from the json file and loaded it
+let mazeLayout = {
+  "lvl": [
+    [1, 9, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+  ]
+};
 
-
+// load images 
 function preload() {
   grassImg = loadImage("clover 1.png");
   pavingImg = loadImage("paving 3.png");
   blockToMove = loadImage("block.png");
-  bgMusic = loadSound("TownTheme.mp3");
-  cantWalk = loadSound("lose music 3 - 1_0.wav");
   heroSprite = loadImage("hero.png");
-  level1 = loadJSON("1stLevel.JSON.json");
-
+  finishLine = loadImage("finish.png");
 }
-
 
 function setup() {
-  //make the canvas the largest square that you can...
   if (windowWidth < windowHeight) {
     createCanvas(windowWidth, windowWidth);
-  }
-  else {
+  } else {
     createCanvas(windowHeight, windowHeight);
   }
+  // set up custom level to play 
+  grid = generateGridFromLayout(mazeLayout.lvl);
+  cellSize = width / GRID_SIZE;
 
-  //if randomizing the grid, do this:
-  grid = generateRandomGrid(GRID_SIZE, GRID_SIZE );
-  
-  //this is dumb -- should check if this is the right size!
-  cellSize = height/grid.length;
 
-  //add player to the grid
-  grid[player.y][player.x] = PLAYER;
-
-  grid[goal.y][goal.x] = GOAL;
 }
-
-function windowResized() {
-  //make the canvas the largest square that you can...
-  if (windowWidth < windowHeight) {
-    resizeCanvas(windowWidth, windowWidth);
-  }
-  else {
-    resizeCanvas(windowHeight, windowHeight);
-  }
-
-  cellSize = height/grid.length;
-}
-
+// putting the correct state to play 
 function draw() {
-  if(state === "start screen"){
-    background("black");
-  }
-  else if (state === "game"){
+  if (state === "start screen") {
+    drawStartScreen();
+  } else if (state === "playing") {
     background(220);
-    displayGrid();
+    drawGrid();
+    drawPlayer();
+    checkForEnd();
+  } else if (state === "game over") {
+    GameOver();
+  } else if (state === "you win") {
+    drawYouWin();
   }
+}
+
+function drawStartScreen() {
+  background(0);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textSize(36);
+  text("Press Enter to Start", width / 2, height / 2);
 }
 
 function keyPressed() {
-  if (key === "r") {
-    grid = generateRandomGrid(GRID_SIZE, GRID_SIZE);
+  if (state === "start screen" && keyCode === ENTER) {
+    state = "playing";
+  } else if (state === "game over" || state === "you win") {
+    if (keyCode === ENTER) {
+      state = "start screen";
+    }
   }
 
-  if (key === "e") {
-    grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  }
-
-  if (key === "w") {   //up
-    movePlayer(player.x + 0, player.y - 1); //0 on x axis, -1 on y axis
-  }
-
-  if (key === "s") {   //down
-    movePlayer(player.x + 0, player.y + 1); //0 on x axis, 1 on y axis
-  }
-
-  if (key === "d") {   //right
-    movePlayer(player.x + 1, player.y + 0); //1 on x axis, 0 on y axis
-  }
-
-  if (key === "a") {   //left
-    movePlayer(player.x - 1, player.y + 0); //-1 on x axis, 0 on y axis
-  }
-  if (key === " " && state === "start screen"){
-    state = "game";
-    bgMusic.loop();
-  }
-  if (key === "1"){
-    grid = level1;
+  if (state === "playing") {
+    movePlayer();
   }
 }
 
-function movePlayer(x, y) {
-  // Don't move off the grid
-  if (x < GRID_SIZE && y < GRID_SIZE && x >= 0 && y >= 0) {
-    // If the target cell is an open tile or contains the block to move
-    if (grid[y][x] === OPEN_TILE || grid[y][x] === GOAL) {
-      // If the target cell contains the block to move
-      if (grid[y][x] === GOAL) {
-        // Calculate the next position of the block
-        let nextBlockX = x + (x - player.x);
-        let nextBlockY = y + (y - player.y);
+// used the grid to map out objects in current grid
+function generateGridFromLayout(layout) {
+  let newGrid = [];
+  for (let i = 0; i < layout. length; i++) {
+    newGrid.push([]);
+    for (let j = 0; j < layout[i]. length;  j++) {
+      if (layout[i][j] ===IMPASSIBLE) {
+        newGrid[i].push (IMPASSIBLE );
+      } else if (layout[i][j] ===GOAL) {
+        newGrid[i].push(GOAL);
+      } else if (layout[i] [j]=== FINISH) {
+        newGrid[i].push(FINISH);
+      } else if (layout[i][j]  === BLOCK) {
+        newGrid[i].push(BLOCK);
+      } else if (layout[i][j]=== FINISHLINE) { 
+        newGrid[i].push( FINISHLINE);
+      } else {
+        newGrid[i].push(OPEN_TILE);
+      }
+    }
+  }
+  return newGrid;
+}
 
-        // Ensure the next position of the block is within the grid and is an open tile
-        if (nextBlockX >= 0 && nextBlockX < GRID_SIZE && nextBlockY >= 0 && nextBlockY < GRID_SIZE && grid[nextBlockY][nextBlockX] === OPEN_TILE) {
-          // Move the block
-          grid[nextBlockY][nextBlockX] = GOAL;
-          grid[y][x] = OPEN_TILE;
-        } else {
-          // If the block can't be moved, play sound or handle accordingly
-          cantWalk.loop(); 
-          return;
+
+// putting the grid in the correct position with correct data
+function drawGrid() {
+  for (let i = 0; i < GRID_SIZE; i++) {
+    for (let j = 0; j < GRID_SIZE; j++) {
+      let x = i * cellSize;
+      let y = j * cellSize;
+
+      if (grid[i][j] === OPEN_TILE) {
+        image(grassImg, x, y, cellSize, cellSize);
+      } else if (grid[i][j] === IMPASSIBLE) {
+        image(pavingImg, x, y, cellSize, cellSize);
+      } else if (grid[i][j] === GOAL) {
+        image(blockToMove, x, y, cellSize, cellSize);
+      } else if (grid[i][j] === BLOCK) { 
+        image(blockToMove, x, y, cellSize, cellSize);
+      } else if (grid[i][j] === FINISHLINE) {
+        image(finishLine, x, y, cellSize, cellSize);
+      }
+    }
+  }
+}
+
+function drawPlayer() {
+  let x = player.x * cellSize;
+  let y = player.y * cellSize;
+  image(heroSprite, x, y, cellSize, cellSize);
+}
+
+// using arrow keys tomove 
+function movePlayer() {
+  let newX = player.x;
+  let newY = player.y;
+
+  if (keyIsDown(LEFT_ARROW)) {
+    newX -= 1;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    newX += 1;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    newY -= 1;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    newY += 1;
+  }
+  if (newX >= 0 && newX < GRID_SIZE && newY >= 0 && newY < GRID_SIZE) {
+    if (grid[newX][newY] === OPEN_TILE || grid[newX][newY] === GOAL) {
+      player.x = newX;
+      player.y = newY;
+      if (grid[player.x][player.y] === GOAL) {
+        grid[player.x][player.y] = OPEN_TILE;
+      }
+    } else if (grid[newX][newY] === BLOCK) { 
+      let newBlockX = block.x + (newX - player.x);
+      let newBlockY = block.y + (newY - player.y);
+      if (newBlockX >= 0 && newBlockX < GRID_SIZE && newBlockY >= 0 && newBlockY < GRID_SIZE) {
+        if (grid[newBlockX][newBlockY] === OPEN_TILE) {
+          block.x = newBlockX;
+          block.y = newBlockY;
+          player.x = newX;
+          player.y = newY;
         }
       }
-      
-
-      // Move the player
-      let oldX = player.x;
-      let oldY = player.y;
-      player.x = x;
-      player.y = y;
-      grid[oldY][oldX] = OPEN_TILE;
-      grid[player.y][player.x] = PLAYER;
+    } else if (grid[newX][newY] === FINISHLINE) { 
+      state = "you win";
     } else {
-      // If the target cell is impassible, play sound or handle accordingly
-      cantWalk.loop(); 
+      cantWalk.play();
     }
   }
 }
-
-
-
-function mousePressed() {
-  let x = Math.floor(mouseX/cellSize);
-  let y = Math.floor(mouseY/cellSize);
-
-  //toggle self
-  toggleCell(x, y);
-}
-
-function toggleCell(x, y) {
-  // make sure the cell you're toggling is in the grid...
-  if (x < GRID_SIZE && y < GRID_SIZE &&
-      x >= 0 && y >= 0) {
-    //toggle the color of the cell
-    if (grid[y][x] === OPEN_TILE) {
-      grid[y][x] = IMPASSIBLE;
-    }
-    else if (grid[y][x] === IMPASSIBLE) {
-      grid[y][x] = OPEN_TILE;
-    }
+// check if at end 
+function checkForEnd() {
+  if (player.x === finish.x && player.y === finish.y) {
+    state = "you win";
   }
 }
 
-function displayGrid() {
-  for (let y = 0; y < grid.length; y++) {
-    for (let x = 0; x < grid[y].length; x++) {
-      if (grid[y][x] === IMPASSIBLE) {
-        // fill("black");
-        image(grassImg, x * cellSize, y * cellSize, cellSize);
-      }
-      else if (grid[y][x] === OPEN_TILE) {
-        // fill("white");
-        image(pavingImg, x * cellSize, y * cellSize, cellSize);
-      }
-      if (grid[y][x] === PLAYER) {
-        image(pavingImg, x * cellSize, y * cellSize, cellSize);
-        image(heroSprite,x* cellSize, y*cellSize,cellSize);
-      }
-      if (grid[y][x] === GOAL){
-        image(pavingImg, x * cellSize, y * cellSize, cellSize);
-        image(blockToMove, x* cellSize,y*cellSize,cellSize);
-      }
-    }
-  }
+// mechanism for end of game 
+function GameOver() {
+  background(0);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textSize(36);
+  text("Game Over", width / 2, height / 2);
+  text("Press Enter to Restart", width / 2, height / 2 + 50);
 }
 
-
-function generateRandomGrid(cols, rows) {
-  let emptyArray = [];
-  for (let y = 0; y < rows; y++) {
-    emptyArray.push([]);
-    for (let x = 0; x < cols; x++) {
-      //half the time, be a 1. Other half, be a 0.
-      if (random(100) < 50) {
-        emptyArray[y].push(0);
-      }
-      else {
-        emptyArray[y].push(1);
-      }
-    }
-  }
-  return emptyArray;
-}
-
-function generateEmptyGrid(cols, rows) {
-  let emptyArray = [];
-  for (let y = 0; y < rows; y++) {
-    emptyArray.push([]);
-    for (let x = 0; x < cols; x++) {
-      emptyArray[y].push(0);
-    }
-  }
-  return emptyArray;
+// if you win, draw againd. 
+function drawYouWin() {
+  background(0);
+  textAlign(CENTER, CENTER);
+  fill(255);
+  textSize(36);
+  text("You Win!", width / 2, height / 2);
+  text("Press Enter to Restart", width / 2, height / 2 + 50);
 }
